f53c8dac5cf79da2e2a294dcfe5207222dabc34f
kosmoSAR <santiagoaristizabal.kosmetikon@gmail.com>
2023-09-18 21:59:27 -0500

    register y login

--- src/app/modules/auth/components/register-form/register-form.component.ts
+++ src/app/modules/auth/components/register-form/register-form.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
+import { RequestStatus } from '@models/status.model';
+import { AuthService } from '@services/auth.service';
 
 import { CustomValidators } from '@utils/validators';
 
@@ -10,31 +12,76 @@ import { CustomValidators } from '@utils/validators';
   templateUrl: './register-form.component.html',
 })
 export class RegisterFormComponent {
+  formUser = this.formBuilder.nonNullable.group({
+    email: ['', [Validators.email, Validators.required]]
+  })
+
   form = this.formBuilder.nonNullable.group({
     name: ['', [Validators.required]],
     email: ['', [Validators.email, Validators.required]],
-    password: ['', [Validators.minLength(6), Validators.required]],
+    password: ['', [Validators.minLength(8), Validators.required]],
     confirmPassword: ['', [Validators.required]],
   }, {
     validators: [ CustomValidators.MatchValidator('password', 'confirmPassword') ]
   });
-  status: string = 'init';
+
+  status: RequestStatus = 'init';
+  statusUser: RequestStatus = 'init';
+  message!: string;
   faEye = faEye;
   faEyeSlash = faEyeSlash;
   showPassword = false;
+  showRegister = false;
 
   constructor(
     private formBuilder: FormBuilder,
-    private router: Router
+    private router: Router,
+    private authService: AuthService
   ) {}
 
   register() {
     if (this.form.valid) {
       this.status = 'loading';
       const { name, email, password } = this.form.getRawValue();
-      console.log(name, email, password);
+      this.authService.registerAndLogin(email, password, name).subscribe({
+        next: () => {
+          this.status = 'success';
+          this.router.navigate(['/app'])
+        },
+        error: (error) => {
+          this.status = 'failed'
+          if (error.error.code === 'SQLITE_CONSTRAINT_UNIQUE'){
+            this.message = 'This user already exists. Do you want to Login?';
+          }
+        }
+      })
     } else {
       this.form.markAllAsTouched();
+    }
+  }
+
+  validateUser(){
+    if (this.formUser.valid) {
+      this.statusUser = 'loading';
+      const { email } = this.formUser.getRawValue();
+      this.authService.isAvailable(email).subscribe({
+        next: (rta) => {
+          this.statusUser = 'success';
+          if (rta.isAvailable) {
+            this.form.controls.email.setValue(email);
+            this.showRegister = true
+          } else {
+            this.router.navigate(['/login'], {
+              queryParams: { email }
+            });
+          }
+        },
+        error: (error) => {
+          this.statusUser = 'failed'
+        }
+      })
+    } else {
+      this.formUser.markAllAsTouched();
     }
   }
 }
---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,34 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from '@environments/environment';
+import { switchMap } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  apiUrl = environment.API_URL;
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  login(email: string, password: string){
+    return this.http.post(`${this.apiUrl}/api/v1/auth/login`, {email, password});
+  }
+
+  register(email: string, password: string, name: string){
+    return this.http.post(`${this.apiUrl}/api/v1/auth/register`, {email, password, name})
+  }
+
+  registerAndLogin(email: string, password: string, name: string){
+    return this.register(name, email, password).pipe(
+      switchMap( () => this.login(email, password))
+    )
+  }
+
+  isAvailable(email: string){
+    return this.http.post<{isAvailable:boolean}>(`${this.apiUrl}/api/v1/auth/is-available`, {email})
+  }
+}
