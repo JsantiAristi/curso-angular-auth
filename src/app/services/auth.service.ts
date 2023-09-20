7738eaf2872e2d95d5801154dd686b14560de474
kosmoSAR <santiagoaristizabal.kosmetikon@gmail.com>
2023-09-19 22:06:47 -0500

    recovery password y token

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,7 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '@environments/environment';
-import { switchMap } from 'rxjs/operators';
+import { switchMap, tap } from 'rxjs/operators';
+import { TokenService } from '@services/token.service';
+import { ResponseLogin } from '@models/auth.model';
 
 @Injectable({
   providedIn: 'root'
@@ -11,11 +13,16 @@ export class AuthService {
   apiUrl = environment.API_URL;
 
   constructor(
-    private http: HttpClient
+    private http: HttpClient,
+    private tokenService: TokenService
   ) { }
 
   login(email: string, password: string){
-    return this.http.post(`${this.apiUrl}/api/v1/auth/login`, {email, password});
+    return this.http.post<ResponseLogin>(`${this.apiUrl}/api/v1/auth/login`, {email, password}).pipe(
+      tap( response => {
+        this.tokenService.saveToken(response.acces_token)
+      })
+    );
   }
 
   register(email: string, password: string, name: string){
@@ -30,5 +37,13 @@ export class AuthService {
 
   isAvailable(email: string){
     return this.http.post<{isAvailable:boolean}>(`${this.apiUrl}/api/v1/auth/is-available`, {email})
+  }
+
+  recovery(email: string){
+    return this.http.post(`${this.apiUrl}/api/v1/auth/recovery`, {email})
+  }
+
+  changePassword(token: string, newPassword: string){
+    return this.http.post(`${this.apiUrl}/api/v1/auth/change-password`, {token, newPassword})
   }
 }
